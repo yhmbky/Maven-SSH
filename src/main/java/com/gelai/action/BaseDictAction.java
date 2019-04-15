78d1e6b8b44b8f32237532145eec03e82e3199a3
tom_glb <goodMorning_glb@itgelai.com>
2019-04-15 18:15:14 +0800

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I00a4b14e7659b205e761b9d603f713cd8aed40c6

--- src/main/java/com/gelai/action/BaseDictAction.java
+++ src/main/java/com/gelai/action/BaseDictAction.java
@@ -47,7 +47,7 @@ public class BaseDictAction extends ActionSupport implements ModelDriven<BaseDic
 		JSONArray jsonArray = JSONArray.fromObject(list,jsonConfig);
 		//将json打印到页面中
 		HttpServletResponse response = ServletActionContext.getResponse();
-		response.setContentType("text/html; charset=UTF-8");System.out.println("......");
+		response.setContentType("text/html; charset=UTF-8");System.out.println("........");
 		try {
 			response.getWriter().print(jsonArray.toString());
 		} catch (IOException e) {
